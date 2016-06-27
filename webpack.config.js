98d81a3b0aac21f01116aae3138b4dcd47194295
cbilliau <christopher@billiau.com>
2016-06-27 08:21:25 -0400

    Add UglifyJS to webpack config file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
+// Needed in order to call plugin
+var webpack = require('webpack');
+
+
 module.exports = {
+    // Defines entry point for webpack to find requriements
     entry: "./js/mvc.js",
+    // Where wepack outputs the resulting file
     output: {
         path: __dirname +"/js",
         filename: "app.js"
     },
+    // Plugins for webpack
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ],
+    // Modules used by webpack
     module: {
       loaders: [
         {
